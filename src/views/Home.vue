ad1057aa1a868de4d7a76cb9141ea5775f12da61
DaZiDian <113085496+DaZiDian@users.noreply.github.com>
2025-12-27 15:48:11 +0800

    Add Rust and Go badges to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -71,6 +71,8 @@
               <img src="https://img.shields.io/badge/JavaScript-F7DF1E?logo=javascript&logoColor=black" alt="JavaScript" class="h-8" />
               <img src="https://img.shields.io/badge/C%23-239120?logo=c-sharp&logoColor=white" alt="C#" class="h-8" />
               <img src="https://img.shields.io/badge/C++-00599C?logo=c%2B%2B&logoColor=white" alt="C++" class="h-8" />
+              <img src="https://img.shields.io/badge/Rust-000000?logo=rust&logoColor=white" alt="Rust" class="h-8" />
+              <img src="https://img.shields.io/badge/Go-00b0d6?logo=go&logoColor=white" alt="Go" class="h-8" />
               <img src="https://img.shields.io/badge/Vue.js-4FC08D?logo=vue.js&logoColor=white" alt="Vue.js" class="h-8" />
               <img src="https://img.shields.io/badge/Python-3776AB?logo=python&logoColor=white" alt="Python" class="h-8" />
               <img src="https://img.shields.io/badge/TypeScript-3178C6?logo=typescript&logoColor=white" alt="TypeScript" class="h-8" />
